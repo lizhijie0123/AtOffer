821cbc76be0b6967dc681b05d47d9c5b07bffb73
lizhijie0123 <lizhije0123@sina.com>
2017-03-15 19:04:59 +0800

    T3

---
+++ Array_T3/Array_T3.cpp
@@ -0,0 +1,35 @@
+// Array_T3.cpp : 定义控制台应用程序的入口点。
+//
+
+//  面试题3：二维数组中的查找
+//  在一个二维数组中，每一行按照从左到右递增，每一列按照从上到下递增的顺序排列。
+
+
+#include "stdafx.h"
+
+// 输入一个这样的二维数组和一个整数。输出：判断数组中是否含有该整数。
+//输入：二维数组和整数
+bool Find(int* matrix, int rows, int columns, int number)
+{
+	bool found = false;
+
+	if (matrix != NULL && rows > 0 && columns > 0)
+	{
+		int row = 0;
+		int column = 0;
+	}
+}
+
+//从第一行开始从左到右比较大小
+
+
+
+
+
+int main()
+{
+
+
+    return 0;
+}
+
